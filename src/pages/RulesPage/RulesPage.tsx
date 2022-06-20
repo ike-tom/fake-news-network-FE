4f446f33e3e1131e7781453c1a47dbf9665b66bd
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-06-20 22:54:40 +0200

    feat: add RulesPage component

--- src/pages/RulesPage/RulesPage.tsx
+++ src/pages/RulesPage/RulesPage.tsx
@@ -7,8 +7,10 @@ export function RulesPage() {
       <Navbar />
       <div className={styles.rulesDescriptionContainer}>
         <p className={styles.rulesDescription}>
-          Na początek wybierz kategorię z której chcesz się sprawdzić. Masz 10 pytań. Odpowiedz na jak najwięcej z nich i
-          znajdź się wśród najlepszych. Dowiedz się na jaką kategorię fake newsów musisz zwrócić szczególną uwagę.
+          Na początek wybierz kategorię z której chcesz się sprawdzić. Masz 10
+          pytań. Odpowiedz na jak najwięcej z nich i znajdź się wśród
+          najlepszych. Dowiedz się na jaką kategorię fake newsów musisz zwrócić
+          szczególną uwagę.
         </p>
       </div>
     </div>
