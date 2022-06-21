94061a7fbd9cbdaf3a704956cf54d4b4b542cb7e
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-06-21 23:17:18 +0200

    refactor: GameView component

--- src/pages/GameView/GameView.tsx
+++ src/pages/GameView/GameView.tsx
@@ -1,16 +1,25 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import styles from "./GameView.module.scss";
 import { Navbar } from "../../components/Navbar/Navbar";
-import { Link, useNavigate } from "react-router-dom";
-import GameOver from "../GameOver/GameOver";
+import { useNavigate } from "react-router-dom";
+import { GameOver } from "../GameOver/GameOver";
 import { useApiGet } from "../../hooks/useApi";
+import { Box, Modal, Typography, Link, CircularProgress } from "@mui/material";
+import { GameModeContext, LoadingContext } from "../../App";
+import { SCORE_PAGE } from "../../urls/frontend";
 
-interface GameViewProps{
+interface GameViewProps {
   score: number;
-  setScore: Dispatch<SetStateAction<number>>
+  setScore: Dispatch<SetStateAction<number>>;
   questID: number;
-  setQuestId: Dispatch<SetStateAction<number>>
-  setIds: Dispatch<SetStateAction<number>>
+  setQuestId: Dispatch<SetStateAction<number>>;
+  setIds: Dispatch<SetStateAction<number>>;
 }
 
 type questionProps = {
@@ -21,53 +30,93 @@ type questionProps = {
   answers: {
     content: string;
     correct: boolean;
-  }[]
+  }[];
   _id: number;
-}
+};
 
-function GameView({score, setScore, questID, setQuestId, setIds}: GameViewProps) {
-  const [questions, setQuestions] = useState<questionProps[]>([])
+export function GameView({
+  score,
+  setScore,
+  questID,
+  setQuestId,
+  setIds,
+}: GameViewProps) {
+  const [questions, setQuestions] = useState<questionProps[]>([]);
   const { data, isFetched } = useApiGet({ path: `questions` });
+  const [open, setOpen] = useState(false);
+  const game = useContext(GameModeContext);
+  const loadingState = useContext(LoadingContext);
+  const [answer, setAnswer] = useState<boolean | null>(null);
+  const navigate = useNavigate();
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setQuestId(questID + 1);
+  };
 
   useEffect(() => {
-    if(data) {
-      const fetchedQuest = data.data
-      const shuffled = fetchedQuest.sort(() => 0.5 - Math.random());
-      setQuestions(shuffled);
+    loadingState.setIsLoading(true);
+
+    if (data) {
+      const fetchedQuest = data.data;
+      const filteredQuestByGameMode = fetchedQuest.filter(
+        (quest: { category: string }) =>
+          quest.category.toLowerCase() === game.gameMode.toLowerCase()
+      );
+
+      if (game.gameMode !== "MIX") {
+        const shuffled = filteredQuestByGameMode.sort(
+          () => 0.5 - Math.random()
+        );
+        setQuestions(shuffled);
+        loadingState.setIsLoading(false);
+      } else {
+        const shuffled = fetchedQuest.sort(() => 0.5 - Math.random());
+        setQuestions(shuffled);
+        loadingState.setIsLoading(false);
+      }
     }
   }, [isFetched]);
 
-
-  const [answer, setAnswer] = useState<boolean | null>(null);
-  const navigate = useNavigate();
- 
   const handleClickTrue = () => {
-    setQuestId(questID + 1);
     if (answer === true) {
       setScore(score + 1);
     }
-    <GameOver score={score}/>
+
+    <GameOver score={score} />;
     if (questID === questions.length - 1) {
-      navigate("/score")
+      navigate(SCORE_PAGE);
     }
   };
 
   const handleClickFalse = () => {
-    setQuestId(questID + 1);
     if (answer === false) {
       setScore(score + 1);
     }
     if (questID === questions.length - 1) {
-      <GameOver score={score}/>
-      navigate("/score")
+      <GameOver score={score} />;
+      navigate(SCORE_PAGE);
     }
   };
 
   return (
     <div className={styles.GameView}>
       <Navbar />
+      {loadingState.isLoading ? (
+        <CircularProgress
+          sx={{
+            color: "white",
+            position: "absolute" as "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            display: "block",
+          }}
+        />
+      ) : null}
 
-      {questions.map(({content, answers, _id}, id) => {
+      {questions.map(({ content, answers, _id, explanation, link }, id) => {
         if (questID === id && questID < 10) {
           setIds(_id);
           if (answers[0].correct !== answer) {
@@ -75,31 +124,75 @@ function GameView({score, setScore, questID, setQuestId, setIds}: GameViewProps)
           }
           return (
             <div key={id.toString()}>
-              <div>
-                <h1 className={styles.title}>Question {id + 1}</h1>
+              <div className={styles.questionWrapper}>
+                <h1 className={styles.title}>Pytanie {id + 1}</h1>
                 <p className={styles.information}>{content}</p>
-                <p className={styles.note}><Link to={'/answer_clarification'}>Wyjaśnienie</Link></p>
+                <Modal open={open} onClose={handleClose}>
+                  <Box
+                    sx={{
+                      position: "absolute" as "absolute",
+                      top: "50%",
+                      left: "50%",
+                      transform: "translate(-50%, -50%)",
+                      width: 400,
+                      bgcolor: "background.paper",
+                      border: "2px solid #000",
+                      boxShadow: 24,
+                      p: 4,
+                      textAlign: "center",
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        marginBottom: 4,
+                      }}
+                      variant="h6"
+                      component="h2"
+                    >
+                      {explanation}
+                    </Typography>
+                    <Link
+                      sx={{
+                        textDecoration: "none",
+                        color: "text.primary",
+                        fontFamily: "Roboto",
+                      }}
+                      href={link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Czytaj więcej
+                    </Link>
+                  </Box>
+                </Modal>
               </div>
               <div className={styles.buttons}>
-                <button className={styles.buttonTrue} onClick={handleClickTrue}>
+                <button
+                  className={styles.buttonTrue}
+                  onClick={() => {
+                    handleOpen();
+                    handleClickTrue();
+                  }}
+                >
                   Prawda
                 </button>
                 <button
                   className={styles.buttonFalse}
-                  onClick={handleClickFalse}
+                  onClick={() => {
+                    handleOpen();
+                    handleClickFalse();
+                  }}
                 >
                   Fałsz
                 </button>
               </div>
             </div>
           );
-        } 
-        if(questID > 9) {
-          navigate("/score")
+        }
+        if (questID > 9) {
+          navigate(SCORE_PAGE);
         }
       })}
     </div>
   );
 }
-
-export default GameView;
