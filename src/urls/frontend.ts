ff6ccefe5ac92891a153b1d8cad9c77ca85a527d
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-06-21 23:18:51 +0200

    refactor: ulrs

--- src/urls/frontend.ts
+++ src/urls/frontend.ts
@@ -1,1 +1,6 @@
 export const NEW_QUESTION_PAGE = "/new-question";
+export const TOP_SCORES_PAGE = "/top_scores";
+export const GAME_MODE_PAGE = "/game_mode";
+export const QUIZ_PAGE = "/quiz";
+export const SCORE_PAGE = "/score";
+export const RULES_PAGE = "/rules";
