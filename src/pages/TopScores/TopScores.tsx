8088172312d741d5f26e5d7debd8c8dbe3d1f446
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-06-21 23:17:41 +0200

    refactor: TopScores component

--- src/pages/TopScores/TopScores.tsx
+++ src/pages/TopScores/TopScores.tsx
@@ -1,19 +1,81 @@
+import { Button, CircularProgress } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { GameModeContext, LoadingContext } from "../../App";
 import { Card } from "../../components/Card/Card";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { TopScoresTable } from "../../components/TopScoresTable/TopScoresTable";
+import { useApiGet } from "../../hooks/useApi";
+import { SCORES_ENDPOINT } from "../../urls/api";
 
 import styles from "./TopScores.module.scss";
 
-const data = (
-  <div className={styles.glassPadding}>
-    <h3 className={styles.hofTitle}>Hall of Fame</h3>
-    <div className={styles.scoresTable}>
-      <TopScoresTable />
-    </div>
-  </div>
-);
+const gameModes = [
+  "Polityka",
+  "Społeczeństwo",
+  "Technologia",
+  "Zdrowie",
+  "CodersCamp",
+  "Mix",
+];
+
+export interface Highscore {
+  username: string;
+  category: string;
+  score: number;
+}
 
 export function TopScores() {
+  const game = useContext(GameModeContext);
+  const loadingState = useContext(LoadingContext);
+  const [highscores, setHighscores] = useState<Highscore[]>([]);
+
+  const changeGameMode = (e: React.MouseEvent<HTMLButtonElement>) => {
+    game.setGameMode(e.currentTarget.innerText);
+  };
+
+  const { data: payload } = useApiGet({ path: SCORES_ENDPOINT });
+
+  useEffect(() => {
+    loadingState.setIsLoading(true);
+
+    if (payload) {
+      const filteredPayload = payload.data.filter(
+        (score: Highscore) => score.category === game.gameMode
+      );
+      const sortedPayload = filteredPayload.sort(
+        (a: Highscore, b: Highscore) => {
+          return b.score - a.score;
+        }
+      );
+      setHighscores(sortedPayload);
+      loadingState.setIsLoading(false);
+    }
+  }, [payload, game.gameMode]);
+
+  const data = (
+    <div className={styles.glassPadding}>
+      <h3 className={styles.hofTitle}>Hall of Fame</h3>
+      <div className={styles.gameModeContainer}>
+        {gameModes.map((gameMode, index) => (
+          <Button
+            key={index}
+            className={styles.gameModeButton}
+            onClick={changeGameMode}
+          >
+            {gameMode}
+          </Button>
+        ))}
+      </div>
+      {loadingState.isLoading ? (
+        <CircularProgress className={styles.progressCircle} />
+      ) : (
+        <div className={styles.scoresTable}>
+          <TopScoresTable highscores={highscores} />
+        </div>
+      )}
+    </div>
+  );
+
   return (
     <div>
       <Navbar />
